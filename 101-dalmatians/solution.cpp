78e12263d23747b6f9b9ad3d3e1b924c42856dd9
Derek McCrary <68666928+dmac90712@users.noreply.github.com>
2026-08-09 16:00:26 -0700

    Night 2: CodeWars - 101 Dalmatians

---
+++ 101-dalmatians/solution.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <string>
+
+std::string howManyDalmatians(int number) {
+    std::vector<std::string> dogs = {
+        "Hardly any",
+        "More than a handful!",
+        "Woah that's a lot of dogs!",
+        "101 DALMATIANS!"
+    };
+    return number == 101 ? dogs[3]
+         : number < 10   ? dogs[0]
+         : number <= 50  ? dogs[1]
+         : dogs[2];
+}
