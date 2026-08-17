f0f7e117ace7afe6242c31b0817e6b3b74da865e
Derek McCrary <68666928+dmac90712@users.noreply.github.com>
2026-08-17 16:06:47 -0700

    Night 10: CodeWars - Return to Sanity

---
+++ return-to-sanity/solution.cpp
@@ -0,0 +1,7 @@
+#include <string>
+#include <unordered_map>
+
+std::unordered_map<std::string, std::string> mystery() {
+    std::unordered_map<std::string, std::string> results = {{"sanity", "Hello"}};
+    return results;
+}
